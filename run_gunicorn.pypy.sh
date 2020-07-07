69dcbce025a39a27d094c6a44618615e2f85bc20
John <john@sukiyaki.su>
2020-07-08 03:50:10 +0800

    增加tornado

--- run_gunicorn.pypy.sh
+++ run_gunicorn.pypy.sh
@@ -2,4 +2,4 @@
 cd $(dirname $0)
 pidfile="/tmp/${1}.pid"
 /opt/.pyenv/versions/pypy3.6-7.3.1/bin/gunicorn -c conf.py -w ${2} mio.passenger_wsgi:app \
-  -k 'eventlet' --pid ${pidfile} --max-requests 20000
+  -k 'tornado' --pid ${pidfile} --max-requests 20000
