fd61406d23fde284dfd9565050a3f9ce96891c86
John <john@sukiyaki.su>
2020-09-14 04:33:53 +0800

    更新脚本

--- run_gunicorn.mac.sh
+++ run_gunicorn.mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 pidfile="/tmp/${1}.pid"
-gunicorn -w ${2} mio.pymio:mWSGI -k 'tornado' --pid ${pidfile} --max-requests 20000
+gunicorn --workers=${2} mio.pymio:mWSGI --worker-class='tornado' --pid=${pidfile} --max-requests=20000 --bind=0.0.0.0:8000
--- run_gunicorn.pypy.sh
+++ run_gunicorn.pypy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)
 pidfile="/tmp/${1}.pid"
-MIO_CONFIG="production" /opt/.pyenv/versions/pypy3.6-7.3.1/bin/gunicorn -w ${2} mio.pymio:mWSGI \
-  -k 'tornado' --pid ${pidfile} --max-requests 20000
+MIO_CONFIG="production" /opt/.pyenv/versions/pypy3.6-7.3.1/bin/gunicorn --workers=${2} mio.pymio:mWSGI \
+  --worker-class='tornado' --pid=${pidfile} --max-requests=20000
