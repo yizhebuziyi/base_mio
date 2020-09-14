a5edfc6a65ffa07e00c4f698971aaa98463bec3b
John <john@sukiyaki.su>
2020-09-14 19:03:28 +0800

    修复cli小bug

--- mio/shell.py
+++ mio/shell.py
@@ -5,12 +5,12 @@ import sys
 
 root_path = os.path.abspath(os.path.dirname(__file__) + '/../')
 sys.path.append(root_path)
-from mio.pymio import app
+from mio.pymio import mWSGI
 from flask_script import Manager
 
 from mio.ext.cli import CliCommand
 
-manager = Manager(app)
+manager = Manager(mWSGI)
 manager.add_command('cli', CliCommand)
 
 if __name__ == '__main__':
