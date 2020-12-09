bd8434984efc4b7f19b8a30401ea21c0c8b9c884
oldmos <oldmos@gmail.com>
2020-12-09 20:50:35 +0800

    去除无用的配置

--- config/__init__.py
+++ config/__init__.py
@@ -33,17 +33,6 @@ class Config:
     # 是否使用CORS
     CORS_ENABLE = os.environ.get('MIO_CORS_ENABLE', False)
     CORS_URI = os.environ.get('MIO_CORS_URI', {r"/*": {"origins": "*"}})
-    # 定时任务相关
-    CRON_ENABLE = os.environ.get('MIO_CRON_ENABLE', False)
-    # at: 分 时 日 月 周
-    CRON_SETTING = [
-        {
-            'name': 'RabbitMQ Sender',
-            'file': 'cron.Mq',
-            'class': 'Sender',
-            'at': '* * * * *'
-        }
-    ]
 
     @staticmethod
     def init_app(app):
