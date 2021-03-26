019e3d13fbe43a92f896ab5fb6cab2866cbe1501
0ldm0s <oldmos@gmail.com>
2021-03-26 10:54:27 +0800

    微调脚本

--- run_webserver.sh
+++ run_webserver.sh
@@ -2,6 +2,9 @@ export MIO_CONFIG="production"
 export MIO_PORT=8000
 PYENV_ROOT="/usr/local/.pyenv"
 PYTHON_ROOT="$PYENV_ROOT/shims"
+export PYTHONIOENCODING=utf-8
+export MIO_CONFIG="production"
+export MIO_LIMIT_CPU=1
 cd $(dirname $0)
 work_path=$(pwd)
 nohup ${PYTHON_ROOT}/python ${work_path}/mio/pymio.pyc > www.log 2>&1 &
